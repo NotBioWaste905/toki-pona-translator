1118ead60deec9bf2b57eea7edf3ffc7d3789f05
Varvara Smirnova <72070078+varvara2510@users.noreply.github.com>
2023-11-30 05:24:39 +0300

    Update app.js

--- website/static/js/app.js
+++ website/static/js/app.js
@@ -1,3 +1,28 @@
-window.addEventListener('scroll', e => {
-	document.documentElement.style.setProperty('--scrollTop', `${this.scrollY}px`) // Update method
-})
+// Обновленный JS
+gsap.registerPlugin(ScrollTrigger);
+
+gsap.utils.toArray('.parallax').forEach(layer => {
+    const speed = parseFloat(layer.dataset.speed);
+    const yPosition = -500 * speed;
+
+    gsap.to(layer, {
+        y: yPosition,
+        ease: 'none',
+        scrollTrigger: {
+            trigger: layer,
+            start: 'top top',
+            end: 'bottom top',
+            scrub: 0.5,
+        },
+    });
+});
+
+gsap.to(".layers", {
+    y: -500,
+    scrollTrigger: {
+        trigger: ".layers",
+        start: "top top",
+        end: "bottom top",
+        scrub: 1,
+    },
+});
